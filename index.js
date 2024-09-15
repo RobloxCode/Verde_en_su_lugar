cb81df9f3e3f8be92ee13d4360b7faffe381a4da
RobloxCode <167715447+RobloxCode@users.noreply.github.com>
2024-09-15 13:44:24 -0600

    now show info
    i made that when you click on the tree image, it'll take you to a website where they are describbing that tree, eventually i would like to implement that info into our own page

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ function showResults() {
     div1.classList.add('tree-photo-container');
     div1.innerHTML = `
       <h2 class="tree-name">Jacaranda</h2>
-      <img src="tree-photos/jacaranda.jpg" class="tree-photo">
+      <img src="tree-photos/jacaranda.jpg" class="tree-photo photo-1">
     `;
 
     const div2 = document.createElement('div');
     div2.classList.add('tree-photo-container');
     div2.innerHTML = `
       <h2 class="tree-name">Fresno</h2>
-      <img src="tree-photos/fresno.jpg" class="tree-photo">
+      <img src="tree-photos/fresno.jpg" class="tree-photo photo-2">
     `;
 
     const div3 = document.createElement('div');
     div3.classList.add('tree-photo-container');
     div3.innerHTML = `
       <h2 class="tree-name">Eucalipto</h2>
-      <img src="tree-photos/eucalipto.jpg" class="tree-photo">
+      <img src="tree-photos/eucalipto.jpg" class="tree-photo photo-3">
     `;
 
     treesContainer.append(h1);
@@ -33,7 +33,23 @@ function showResults() {
     treesContainer.append(div2);
     treesContainer.append(div3);
     
+    document.querySelector('.photo-1').addEventListener('click', () => {
+      takeToInfoPage('https://nuestraflora.com/c-arboles/arbol-de-jacaranda/');
+    });
+    
+    document.querySelector('.photo-2').addEventListener('click', () => {
+      takeToInfoPage('https://nuestraflora.com/c-arboles/arbol-fresno/');
+    });
+    
+    document.querySelector('.photo-3').addEventListener('click', () => {
+      takeToInfoPage('https://nuestraflora.com/c-arboles/tipos-de-eucalipto/');
+    });
+
   }
+}
+
+function takeToInfoPage(link) {
+  window.open(link, '_blank');
 }
 
 document.querySelector('.show-trees-button').addEventListener('click', showResults);
